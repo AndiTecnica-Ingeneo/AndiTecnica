b84377572a49cb7e29ad6338085ded4a614cc114
Andres Felipe Pinillos <andres.pinillos@ingeneo.com.co>
2017-05-30 15:38:50 -0500

    Plantillas Base View

---
+++ AndiTecnica.Web/AndiTecnica.Web/Views/Base/Plantilla.aspx
@@ -0,0 +1,92 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/MasterPages/Principal.Master" CodeBehind="Plantilla.aspx.vb" Inherits="AndiTecnica.Web.Plantilla" %>
+<%@ MasterType VirtualPath="~/MasterPages/Principal.Master" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cph_botones" runat="server">
+    <asp:linkbutton cssclass="link_button" id="lkb_buscar" runat="server" tooltip="Buscar"><span class="fa fa-search"></span></asp:linkbutton>
+    <asp:linkbutton cssclass="link_button" id="lkb_agregar" runat="server" tooltip="Agregar"><span class="fa fa-plus"></span></asp:linkbutton>
+    <asp:linkbutton cssclass="link_button" id="lkb_guardar" runat="server" tooltip="Guardar"><span class="fa fa-floppy-o"></span></asp:linkbutton>
+    <asp:linkbutton cssclass="link_button" id="lkb_eliminar" runat="server" tooltip="Eliminar"><span class="fa fa-trash-o"></span></asp:linkbutton>
+    <asp:linkbutton cssclass="link_button" id="lkb_salir" runat="server" tooltip="Volver"><span class="fa fa-undo"></span></asp:linkbutton>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="cph_contenido" runat="server">
+    <asp:panel id="pnl_form" runat="server">
+   <table class="nav-justified">
+      <tr>
+        <td class="quince alinear_der">
+          <asp:Label ID="lbl_Plantilla" runat="server" Text="Plantilla"></asp:Label>
+        </td>
+        <td class="veinticuatro">
+          <asp:TextBox ID="txt_nombres" class="form-control" runat="server"></asp:TextBox>
+        </td>
+        <td class="dos">&nbsp;</td>
+        <td class="veinticuatro alinear_der">
+          <asp:Label ID="lbl_codigo" runat="server" Text="Codigo"></asp:Label>
+        </td>
+        <td class="veinticuatro">
+          <asp:TextBox ID="txt_codigo" runat="server" class="form-control"></asp:TextBox>
+        </td>
+        <td class="dos">&nbsp;</td>
+        <td class="nueve">&nbsp;</td>
+      </tr>
+    </table>
+    <br />
+  </asp:panel>
+
+    <asp:panel id="pnl_list" runat="server">
+    <asp:Panel ID="pnl_buscar" runat="server">
+      <table class="cien">
+        <tr>
+          <td class="veinte"></td>
+          <td class="veinte" align="center">
+            <asp:DropDownList ID="ddl_buscar" runat="server" CssClass="dropdown btn btn-default dropdown-toggle text-center ochenta">
+              <asp:ListItem Value="0">Buscar por</asp:ListItem>
+              <asp:ListItem Value="1">Plantilla</asp:ListItem>
+              <asp:ListItem Value="2">Codigo</asp:ListItem>
+            </asp:DropDownList>
+          </td>
+          <td class="veinte">
+            <asp:TextBox ID="txt_bucar" class="form-control" runat="server"></asp:TextBox>
+          </td>
+          <td class="veinte" align="center">
+            <button type="button" id="btn_filtrar" class="btn btn-default ochenta" runat="server">Filtrar</button>
+
+          </td>
+          <td class="veinte"></td>
+        </tr>
+      </table>
+      <br />
+    </asp:Panel>
+    <asp:GridView ID="grd_Plantillas" runat="server" AutoGenerateColumns="False" DataKeyNames="PlantillaId" DataSourceID="sds_Plantillas" CssClass="table table-bordered" CellPadding="4" ForeColor="#333333" GridLines="None" AllowPaging="True" AllowSorting="True">
+      <AlternatingRowStyle BackColor="White" />
+      <Columns>
+        <asp:CommandField ShowSelectButton="True" ControlStyle-CssClass="fa fa-pencil" ItemStyle-HorizontalAlign="Center" SelectText="">
+          <ControlStyle CssClass="fa fa-pencil link_button_select" />
+        </asp:CommandField>
+        <asp:BoundField DataField="PlantillaId" HeaderText="PlantillaId" InsertVisible="False" ReadOnly="True" SortExpression="PlantillaId" Visible="False" />
+        <asp:BoundField DataField="Nombre" HeaderText="Plantilla" SortExpression="Nombre" />
+        <asp:BoundField DataField="Codigo" HeaderText="Codigo" SortExpression="Codigo" />
+      </Columns>
+      <HeaderStyle ForeColor="White" BackColor="#005490" />
+    </asp:GridView>
+    <asp:SqlDataSource ID="sds_Plantillas" runat="server" ConnectionString="<%$ ConnectionStrings:CVConnectionString %>" SelectCommand="PlantillasListar" SelectCommandType="StoredProcedure">
+      <SelectParameters>
+        <asp:Parameter DefaultValue="  " Name="campo" Type="String" />
+        <asp:Parameter DefaultValue="  " Name="texto" Type="String" />
+      </SelectParameters>
+    </asp:SqlDataSource>
+  </asp:panel>
+
+    <asp:hiddenfield id="hdf_id" runat="server" />
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#mainNav li").removeAttr("class");
+            $("#mainNav li").attr("class scroll-link");
+            $("#li_Plantillas").attr("class", "active");
+        });
+
+    </script>
+
+</asp:Content>
---
+++ AndiTecnica.Web/AndiTecnica.Web/Views/Base/Plantilla.aspx.vb
@@ -0,0 +1,153 @@
+﻿Public Class Plantilla
+    Inherits System.Web.UI.Page
+
+    Public Enum EnumModoPagina
+        Insert
+        Edit
+    End Enum
+
+    Public Property ModoPaginaPlantillas As EnumModoPagina
+        Get
+            Return ViewState("ModoPaginaPlantillas")
+        End Get
+        Set(ByVal value As EnumModoPagina)
+            ViewState("ModoPaginaPlantillas") = value
+        End Set
+    End Property
+
+    Dim PlantillaFacade As New Facades
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not Page.IsPostBack Then
+            MostrarLista()
+            pnl_buscar.Visible = False
+        End If
+    End Sub
+
+    Protected Sub Buscar(sender As Object, e As EventArgs) Handles lkb_buscar.Click
+        pnl_buscar.Visible = True
+        Master.mensajes = Nothing
+    End Sub
+
+    Protected Sub Filtrar(sender As Object, e As EventArgs) Handles btn_filtrar.ServerClick
+        If ddl_buscar.SelectedValue = 0 Then
+            Master.mensajes = "warning;Por favor seleccione el campo a buscar"
+        ElseIf txt_bucar.Text = "" Then
+            Master.mensajes = "warning;Por favor ingrese la informacion a buscar"
+        Else
+            Try
+                sds_Plantillas.SelectParameters.Clear()
+                sds_Plantillas.SelectParameters.Add("campo", ddl_buscar.SelectedValue)
+                sds_Plantillas.SelectParameters.Add("texto", txt_bucar.Text)
+                Master.mensajes = Nothing
+            Catch ex As Exception
+                Master.mensajes = "danger;" & ex.Message
+            End Try
+        End If
+    End Sub
+
+    Protected Sub Agregar(sender As Object, e As EventArgs) Handles lkb_agregar.Click
+        ModoPaginaPlantillas = EnumModoPagina.Insert
+        MostrarFormulario()
+        LimpiarPlantilla()
+    End Sub
+
+    Protected Sub Editar(sender As Object, e As EventArgs) Handles grd_Plantillas.SelectedIndexChanged
+        ModoPaginaPlantillas = EnumModoPagina.Edit
+        MostrarFormulario()
+        LimpiarPlantilla()
+        hdf_id.Value = grd_Plantillas.SelectedValue
+        ObtenerPlantilla()
+    End Sub
+
+    Protected Sub Guardar(sender As Object, e As EventArgs) Handles lkb_guardar.Click
+        Try
+            Dim Plantilla = AsignarPlantilla()
+            If ModoPaginaPlantillas = EnumModoPagina.Insert Then
+                PlantillaFacade.GuardarPlantilla(Plantilla)
+                Master.mensajes = "success;Plantilla creada con exito"
+                OcultarBusqueda()
+                ModoPaginaPlantillas = EnumModoPagina.Edit
+            Else
+                PlantillaFacade.ActualizarPlantilla(Plantilla)
+                Master.mensajes = "success;Plantilla modificada con exito"
+            End If
+            MostrarLista()
+        Catch ex As Exception
+            Master.mensajes = "danger;" & ex.Message
+        End Try
+    End Sub
+
+    Protected Sub Eliminar(sender As Object, e As EventArgs) Handles lkb_eliminar.Click
+        Try
+            PlantillaFacade.EliminarPlantilla(hdf_id.Value)
+            Master.mensajes = "success;Plantilla eliminada con exito"
+            MostrarLista()
+        Catch ex As Exception
+            Master.mensajes = "danger;" & ex.Message
+        End Try
+    End Sub
+
+    Protected Sub Salir(sender As Object, e As EventArgs) Handles lkb_salir.Click
+        MostrarLista()
+        Master.mensajes = Nothing
+    End Sub
+
+    Public Sub MostrarLista()
+        pnl_form.Visible = False
+        pnl_list.Visible = True
+        lkb_buscar.Visible = True
+        lkb_agregar.Visible = True
+        lkb_guardar.Visible = False
+        lkb_eliminar.Visible = False
+        lkb_salir.Visible = False
+        grd_Plantillas.DataBind()
+    End Sub
+
+    Public Sub MostrarFormulario()
+        pnl_form.Visible = True
+        pnl_list.Visible = False
+        lkb_buscar.Visible = False
+        lkb_agregar.Visible = False
+        lkb_guardar.Visible = True
+        lkb_eliminar.Visible = True
+        lkb_salir.Visible = True
+    End Sub
+
+    Public Sub OcultarBusqueda()
+        pnl_buscar.Visible = False
+        ddl_buscar.SelectedValue = 0
+        txt_bucar.Text = ""
+    End Sub
+
+    Public Sub ObtenerPlantilla()
+        Dim Plantilla = PlantillaFacade.ConsultarPlantillaxId(hdf_id.Value)
+        txt_nombres.Text = Plantilla.Nombre
+        txt_codigo.Text = Plantilla.Codigo
+    End Sub
+
+    Public Function AsignarPlantilla() As Plantillas
+        Dim Plantilla As New Plantillas
+        Try
+            Plantilla.PlantillaId = hdf_id.Value
+        Catch ex As Exception
+
+        End Try
+        If txt_nombres.Text = "" Then
+            Throw New Exception("Por favor ingrese un nombre")
+        Else
+            Plantilla.Nombre = txt_nombres.Text
+        End If
+        Plantilla.Codigo = txt_codigo.Text
+        Return Plantilla
+    End Function
+
+    Public Sub LimpiarPlantilla()
+        hdf_id.Value = Nothing
+        txt_nombres.Text = Nothing
+        txt_codigo.Text = Nothing
+        Master.mensajes = Nothing
+        txt_nombres.Focus()
+    End Sub
+
+End Class
