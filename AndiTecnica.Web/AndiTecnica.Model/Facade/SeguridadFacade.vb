2f27a0e61084a0738c92c3dcd781d1b90087a138
Andres Felipe Pinillos <andres.pinillos@ingeneo.com.co>
2017-05-30 17:27:32 -0500

    Agregar Modulo Completo de Perfiles

--- AndiTecnica.Web/AndiTecnica.Model/Facade/SeguridadFacade.vb
+++ AndiTecnica.Web/AndiTecnica.Model/Facade/SeguridadFacade.vb
@@ -1,4 +1,5 @@
-﻿Imports AndiTecnica.Model.BL
+﻿Imports AndiTecnica.Model.BL.Usuarios
+Imports AndiTecnica.Model.BL.Perfiles
 
 Namespace SeguridadFacade
     Public Class SeguridadFacade
@@ -14,6 +15,68 @@ Namespace SeguridadFacade
             End Try
         End Function
 #End Region
+
+
+
+#Region "Perfiles"
+
+        Public Function ListarPerfiles() As List(Of Perfiles)
+            Try
+                Dim bl As New PerfilesBL
+                Return bl.ListarPerfiles
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            End Try
+        End Function
+
+        Public Function BuscarPerfiles(ByVal Nombre As String) As List(Of Perfiles)
+            Try
+                Dim bl As New PerfilesBL
+                Return bl.BuscarPerfiles(Nombre)
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            End Try
+        End Function
+
+        Public Function ConsultarPerfilxId(ByVal Perfilid As Integer) As Perfiles
+            Try
+                Dim bl As New PerfilesBL
+                Return bl.ConsultarPerfilxId(Perfilid)
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            End Try
+        End Function
+
+        Public Sub GuardarPerfil(ByVal Perfil As Perfiles)
+            Try
+                Dim bl As New PerfilesBL
+                Perfil.Creado = Date.Now
+                Perfil.Modificado = Date.Now
+                bl.GuardarPerfil(Perfil)
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            End Try
+        End Sub
+
+        Public Sub ActualizarPerfil(ByVal Perfil As Perfiles)
+            Try
+                Dim bl As New PerfilesBL
+                bl.ActualizarPerfil(Perfil)
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            End Try
+        End Sub
+
+        Public Sub EliminarPerfil(ByVal PerfilId As Integer)
+            Try
+                Dim bl As New PerfilesBL
+                bl.EliminarPerfil(PerfilId)
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            End Try
+        End Sub
+#End Region
+
 
 
     End Class
