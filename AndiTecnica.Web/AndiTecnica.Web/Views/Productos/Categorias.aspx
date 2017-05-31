82057c4611d35317c56be56cf68e167b5e95d5a5
Andres Felipe Pinillos <andres.pinillos@ingeneo.com.co>
2017-05-31 13:30:43 -0500

    Categorias Productos

---
+++ AndiTecnica.Web/AndiTecnica.Web/Views/Productos/Categorias.aspx
@@ -0,0 +1,85 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/MasterPages/Principal.Master" CodeBehind="Categorias.aspx.vb" Inherits="AndiTecnica.Web.Categorias" %>
+<%@ MasterType VirtualPath="~/MasterPages/Principal.Master" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cph_botones" runat="server">
+    <asp:linkbutton cssclass="link_button" id="lkb_buscar" runat="server" tooltip="Buscar"><span class="fa fa-search"></span></asp:linkbutton>
+    <asp:linkbutton cssclass="link_button" id="lkb_agregar" runat="server" tooltip="Agregar"><span class="fa fa-plus"></span></asp:linkbutton>
+    <asp:linkbutton cssclass="link_button" id="lkb_editar" runat="server" tooltip="Editar"><span class="fa fa-pencil"></span></asp:linkbutton>
+    <asp:linkbutton cssclass="link_button" id="lkb_guardar" runat="server" tooltip="Guardar"><span class="fa fa-floppy-o"></span></asp:linkbutton>
+    <asp:linkbutton cssclass="link_button" id="lkb_eliminar" runat="server" tooltip="Eliminar"><span class="fa fa-trash-o"></span></asp:linkbutton>
+    <asp:linkbutton cssclass="link_button" id="lkb_salir" runat="server" tooltip="Volver"><span class="fa fa-undo"></span></asp:linkbutton>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="cph_contenido" runat="server">
+    <asp:panel id="pnl_form" runat="server">
+   <table class="nav-justified">
+      <tr>
+        <td class="quince alinear_der">
+          <asp:Label ID="lbl_Nombre" runat="server" Text="Categorias : "></asp:Label>
+        </td>
+        <td class="veinticuatro">
+          <asp:TextBox ID="txt_nombreCategorias" class="form-control" runat="server"></asp:TextBox>
+        </td>
+        <td class="dos">&nbsp;</td>
+        <td class="veinticuatro alinear_der">
+          <asp:Label ID="lbl_descripcion" runat="server" Text="Descripcion"></asp:Label>
+        </td>
+        <td class="veinticuatro">
+          <asp:TextBox ID="txt_DescripcionCategorias" runat="server" class="form-control"></asp:TextBox>
+        </td>
+        <td class="dos">&nbsp;</td>
+        <td class="nueve">&nbsp;</td>
+      </tr>
+    </table>
+    <br />
+  </asp:panel>
+
+    <asp:panel id="pnl_list" runat="server">
+    <asp:Panel ID="pnl_buscar" runat="server">
+      <table class="cien">
+        <tr>
+          <td class="veinte"></td>
+          <td class="quince alinear_der" align="center">
+              <asp:Label ID="lbl_buscar" runat="server" Text="Buscar Categorias : "></asp:Label>
+          </td>
+          <td class="veinte">
+            <asp:TextBox ID="txt_bucar" class="form-control" runat="server"></asp:TextBox>
+          </td>
+          <td class="veinte" align="center">
+            <button type="button" id="btn_filtrar" class="btn btn-default ochenta" runat="server">Filtrar</button>
+
+          </td>
+          <td class="veinte"></td>
+        </tr>
+      </table>
+      <br />
+    </asp:Panel>
+    <asp:GridView ID="grd_CategoriasProductos" runat="server" DataKeyNames="CategoriasId" AutoGenerateColumns="False" DataSourceID="ods_CategoriasProductos" CssClass="table table-bordered" CellPadding="4" ForeColor="#333333" GridLines="None" AllowPaging="True" AllowSorting="True">
+      <AlternatingRowStyle BackColor="White" />
+      <Columns>
+        <asp:CommandField ShowSelectButton="True" ControlStyle-CssClass="fa fa-pencil" ItemStyle-HorizontalAlign="Center" SelectText=""><ControlStyle CssClass="fa fa-pencil link_button_select"/></asp:CommandField>
+        <asp:BoundField DataField="Nombre" HeaderText="Nombre" SortExpression="Nombre" />
+        <asp:BoundField DataField="Descripcion" HeaderText="Descripcion" SortExpression="Descripcion" />
+        <asp:BoundField DataField="Estado" HeaderText="Estado" SortExpression="Estado" />
+        <asp:BoundField DataField="Creado" HeaderText="Creado" SortExpression="Creado" />
+        <asp:BoundField DataField="Modificado" HeaderText="Modificado" SortExpression="Modificado" />
+      </Columns>
+      <HeaderStyle ForeColor="White" BackColor="#0f2612" />
+    </asp:GridView>
+  </asp:panel>
+
+    <asp:hiddenfield id="hdf_id" runat="server" />
+
+    <asp:ObjectDataSource ID="ods_CategoriasProductos" runat="server" SelectMethod="ListarCategoriasProductos" TypeName="AndiTecnica.Model.ProductosFacade.ProductosFacade"></asp:ObjectDataSource>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#mainNav li").removeAttr("class");
+            $("#mainNav li").attr("class scroll-link");
+            $("#li_Plantillas").attr("class", "active");
+        });
+
+    </script>
+
+</asp:Content>
---
+++ AndiTecnica.Web/AndiTecnica.Web/Views/Productos/Categorias.aspx.vb
@@ -0,0 +1,189 @@
+﻿Imports AndiTecnica.Model
+Imports AndiTecnica.Model.ProductosFacade
+Imports AndiTecnica.Model.SeguridadFacade
+
+Public Class Categorias
+    Inherits System.Web.UI.Page
+
+    Public Shared prm As List(Of AutorizarBotones_Result)
+
+    Public Enum EnumModoPagina
+        Insert
+        Edit
+        Update
+    End Enum
+
+    Public Property ModoPaginaCategoriasProductos As EnumModoPagina
+        Get
+            Return ViewState("ModoPaginaCategoriasProductos")
+        End Get
+        Set(ByVal value As EnumModoPagina)
+            ViewState("ModoPaginaCategoriasProductos") = value
+        End Set
+    End Property
+
+    Dim ProductosFacade As New ProductosFacade
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not Page.IsPostBack Then
+            MostrarLista()
+            pnl_buscar.Visible = False
+            CargarPermisos()
+        End If
+    End Sub
+
+    Protected Sub Buscar(sender As Object, e As EventArgs) Handles lkb_buscar.Click
+        pnl_buscar.Visible = True
+        Master.mensajes = Nothing
+    End Sub
+
+    Protected Sub Filtrar(sender As Object, e As EventArgs) Handles btn_filtrar.ServerClick
+        If txt_bucar.Text = "" Then
+            Master.mensajes = "warning;Por favor ingrese la informacion a buscar"
+        Else
+            Try
+                ods_CategoriasProductos.SelectParameters.Clear()
+                ods_CategoriasProductos.SelectMethod = "BuscarCategoriasProductos"
+                ods_CategoriasProductos.TypeName = "AndiTecnica.Model.SeguridadFacade.SeguridadFacade"
+                ods_CategoriasProductos.SelectParameters.Add("Nombre", txt_bucar.Text)
+                Master.mensajes = Nothing
+            Catch ex As Exception
+                Master.mensajes = "danger;" & ex.Message
+            End Try
+        End If
+    End Sub
+
+    Protected Sub Agregar(sender As Object, e As EventArgs) Handles lkb_agregar.Click
+        ModoPaginaCategoriasProductos = EnumModoPagina.Insert
+        MostrarFormulario()
+        LimpiarCategorias()
+    End Sub
+
+    Protected Sub Editar(sender As Object, e As EventArgs) Handles grd_CategoriasProductos.SelectedIndexChanged
+        ModoPaginaCategoriasProductos = EnumModoPagina.Edit
+        MostrarFormulario()
+        LimpiarCategorias()
+        hdf_id.Value = grd_CategoriasProductos.SelectedValue
+        ObtenerCategorias()
+    End Sub
+
+    Private Sub lkb_editar_Click(sender As Object, e As EventArgs) Handles lkb_editar.Click
+        ModoPaginaCategoriasProductos = EnumModoPagina.Update
+        MostrarFormulario()
+    End Sub
+
+    Protected Sub Guardar(sender As Object, e As EventArgs) Handles lkb_guardar.Click
+        Try
+            Dim Categorias = AsignarCategorias()
+            If ModoPaginaCategoriasProductos = EnumModoPagina.Insert Then
+                ProductosFacade.GuardarCategorias(Categorias)
+                Master.mensajes = "success;Categorias creado con exito"
+                OcultarBusqueda()
+                ModoPaginaCategoriasProductos = EnumModoPagina.Edit
+            Else
+                ProductosFacade.ActualizarCategorias(Categorias)
+                Master.mensajes = "success;Categorias modificado con exito"
+            End If
+            MostrarLista()
+        Catch ex As Exception
+            Master.mensajes = "danger;" & ex.Message
+        End Try
+    End Sub
+
+    Protected Sub Eliminar(sender As Object, e As EventArgs) Handles lkb_eliminar.Click
+        Try
+            ProductosFacade.EliminarCategorias(hdf_id.Value)
+            Master.mensajes = "success;Categorias eliminado con exito"
+            MostrarLista()
+        Catch ex As Exception
+            Master.mensajes = "danger;" & ex.Message
+        End Try
+    End Sub
+
+    Protected Sub Salir(sender As Object, e As EventArgs) Handles lkb_salir.Click
+        MostrarLista()
+        Master.mensajes = Nothing
+    End Sub
+
+    Public Sub MostrarLista()
+        pnl_form.Visible = False
+        pnl_list.Visible = True
+        lkb_buscar.Visible = True
+        lkb_agregar.Visible = True
+        lkb_guardar.Visible = False
+        lkb_editar.Visible = False
+        lkb_eliminar.Visible = False
+        lkb_salir.Visible = False
+        ods_CategoriasProductos.SelectParameters.Clear()
+        ods_CategoriasProductos.SelectMethod = "ListarCategoriasProductos"
+        ods_CategoriasProductos.TypeName = "AndiTecnica.Model.SeguridadFacade.SeguridadFacade"
+        grd_CategoriasProductos.DataBind()
+    End Sub
+
+    Public Sub MostrarFormulario()
+        pnl_form.Visible = True
+        pnl_list.Visible = False
+        lkb_buscar.Visible = False
+        lkb_agregar.Visible = False
+        lkb_salir.Visible = True
+
+        If ModoPaginaCategoriasProductos = EnumModoPagina.Edit Then
+            pnl_form.Enabled = False
+            lkb_eliminar.Visible = True
+            lkb_editar.Visible = True
+            lkb_guardar.Visible = False
+        ElseIf ModoPaginaCategoriasProductos = EnumModoPagina.Insert Or ModoPaginaCategoriasProductos = EnumModoPagina.Update Then
+            pnl_form.Enabled = True
+            lkb_guardar.Visible = True
+            lkb_editar.Visible = False
+        End If
+
+    End Sub
+
+    Public Sub OcultarBusqueda()
+        pnl_buscar.Visible = False
+        txt_bucar.Text = ""
+    End Sub
+
+    Public Sub ObtenerCategorias()
+        Dim Categorias = ProductosFacade.ConsultarCategoriasxId(hdf_id.Value)
+        txt_nombreCategorias.Text = Categorias.Nombre
+        txt_DescripcionCategorias.Text = Categorias.Descripcion
+    End Sub
+
+    Public Function AsignarCategorias() As CategoriasProductos
+        Dim Categorias As New CategoriasProductos
+        Try
+            Categorias.CategoriasId = hdf_id.Value
+        Catch ex As Exception
+
+        End Try
+        If txt_nombreCategorias.Text = "" Then
+            Throw New Exception("Por favor ingrese un nombre")
+        Else
+            Categorias.Nombre = txt_nombreCategorias.Text
+        End If
+        Categorias.Descripcion = txt_DescripcionCategorias.Text
+        Return Categorias
+    End Function
+
+    Public Sub LimpiarCategorias()
+        hdf_id.Value = Nothing
+        txt_nombreCategorias.Text = Nothing
+        txt_DescripcionCategorias.Text = Nothing
+        Master.mensajes = Nothing
+        txt_nombreCategorias.Focus()
+    End Sub
+
+    Protected Sub CargarPermisos()
+        Dim permisos As New SeguridadFacade
+        prm = permisos.AutorizarBotones("CategoriasProductos")
+        For i As Integer = 0 To prm.Count
+            Try
+                Me.Master.FindControl("cph_botones").FindControl(prm.Item(i).Nombre).Visible = prm.Item(i).Valor
+            Catch ex As Exception
+            End Try
+        Next
+    End Sub
+
+End Class
