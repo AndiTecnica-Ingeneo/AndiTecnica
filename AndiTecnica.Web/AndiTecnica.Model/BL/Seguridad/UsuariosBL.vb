fad131a3ce7e0c32bb8f60959ca8d9258cd1172d
Daniel Pinillos <Daniel.Pinillo@outlook.com>
2017-05-31 10:53:00 -0500

    Users

--- AndiTecnica.Web/AndiTecnica.Model/BL/Seguridad/UsuariosBL.vb
+++ AndiTecnica.Web/AndiTecnica.Model/BL/Seguridad/UsuariosBL.vb
@@ -23,65 +23,33 @@ Namespace BL.Usuarios
             End Try
         End Function
 
-        'Public Sub GuardarUsuario(ByVal usuario As Usuarios, ByVal menuseleccionados As List(Of Menu))
-        '    Try
-        '        Dim dao As New UsuariosDao
-        '        Dim menusbl As New MenusBL
-        '        Dim menuxUsuarioBl As New MenusxUsuarioBL
-        '        dao.GuardarUsuario(usuario)
-        '        For i As Integer = 0 To menuseleccionados.Count - 1
-        '            Dim men = menusbl.ConsultarMenuxNombre(menuseleccionados.Item(i).Nombre.ToString)
-        '            Dim menuxusuario As New MenusxUsuario
-        '            menuxusuario.UsuarioFk = usuario.UsuarioId
-        '            menuxusuario.MenuFk = men.MenuId
-        '            menuxusuario.Valor = menuseleccionados.Item(i).Valor
-        '            menuxUsuarioBl.GuardarMenuxUsuario(menuxusuario)
-        '        Next
-        '    Catch ex As Exception
-        '        Throw New Exception(ex.Message)
-        '    End Try
-        'End Sub
+        Public Sub GuardarUsuario(ByVal usuario As Model.Usuarios)
+            Try
+                Dim dao As New UsuariosDao
+                dao.GuardarUsuario(usuario)
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            End Try
+        End Sub
 
-        'Public Sub ActualizarUsuario(ByVal usuario As Usuarios, ByVal menuseleccionados As List(Of Menu))
-        '    Try
-        '        Dim dao As New UsuariosDao
-        '        Dim menusbl As New MenusBL
-        '        Dim menuxUsuarioBl As New MenusxUsuarioBL
-        '        dao.ActualizarUsuario(usuario)
+        Public Sub ActualizarUsuario(ByVal usuario As Model.Usuarios)
+            Try
+                Dim dao As New UsuariosDao
+                dao.ActualizarUsuario(usuario)
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            End Try
+        End Sub
 
-        '        Dim menusxusuario = menuxUsuarioBl.ListarMenusxUsuarioXUsuarioId(usuario.UsuarioId)
-        '        For i As Integer = 0 To menusxusuario.Count - 1
-        '            menuxUsuarioBl.EliminarMenuxUsuario(menusxusuario.Item(i).MenuxUsuarioId)
-        '        Next
-
-        '        For i As Integer = 0 To menuseleccionados.Count - 1
-        '            Dim men = menusbl.ConsultarMenuxNombre(menuseleccionados.Item(i).Nombre.ToString)
-        '            Dim menuxusuario As New MenusxUsuario
-        '            menuxusuario.UsuarioFk = usuario.UsuarioId
-        '            menuxusuario.MenuFk = men.MenuId
-        '            menuxusuario.Valor = menuseleccionados.Item(i).Valor
-        '            menuxUsuarioBl.GuardarMenuxUsuario(menuxusuario)
-        '        Next
-
-        '    Catch ex As Exception
-        '        Throw New Exception(ex.Message)
-        '    End Try
-        'End Sub
-
-        'Public Sub EliminarUsuario(ByVal usuarioId As Integer)
-        '    Try
-        '        Dim dao As New UsuariosDao
-        '        Dim menuxUsuarioBl As New MenusxUsuarioBL
-        '        Dim usuario = ConsultarUsuarioxId(usuarioId)
-        '        Dim menusxusuario = menuxUsuarioBl.ListarMenusxUsuarioXUsuarioId(usuario.UsuarioId)
-        '        For i As Integer = 0 To menusxusuario.Count - 1
-        '            menuxUsuarioBl.EliminarMenuxUsuario(menusxusuario.Item(i).MenuxUsuarioId)
-        '        Next
-        '        dao.EliminarUsuario(usuario)
-        '    Catch ex As Exception
-        '        Throw New Exception(ex.Message)
-        '    End Try
-        'End Sub
+        Public Sub EliminarUsuario(ByVal usuarioId As Integer)
+            Try
+                Dim dao As New UsuariosDao
+                Dim usuario = ConsultarUsuarioxId(usuarioId)
+                dao.EliminarUsuario(usuario)
+            Catch ex As Exception
+                Throw New Exception(ex.Message)
+            End Try
+        End Sub
 
         Public Function IniciarSesion(ByVal usuario As String, ByVal clave As String) As Model.Usuarios
             Try
